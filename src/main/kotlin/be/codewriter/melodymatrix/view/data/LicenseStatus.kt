db807ceb2ae6c162e48ac014c4e5b27caeea5d33
Frank Delporte <frank@webtechie.be>
2024-06-27 17:11:45 +0200

    Get license status from server

--- src/main/kotlin/be/codewriter/melodymatrix/view/data/LicenseStatus.kt
+++ src/main/kotlin/be/codewriter/melodymatrix/view/data/LicenseStatus.kt
@@ -2,5 +2,13 @@ package be.codewriter.melodymatrix.view.data
 
 import javafx.beans.property.BooleanProperty
 import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.StringProperty
 
-data class LicenseStatus(val licenseValidated: BooleanProperty = SimpleBooleanProperty(false))
+data class LicenseStatus(
+    val isValid: BooleanProperty = SimpleBooleanProperty(false),
+    val name: StringProperty = SimpleStringProperty(""),
+    val email: StringProperty = SimpleStringProperty(""),
+    val validTill: StringProperty = SimpleStringProperty(""),
+    val key: StringProperty = SimpleStringProperty("")
+)
