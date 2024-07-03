883849670380e3a38b15f328c88584d42bd967b7
Frank Delporte <frank@webtechie.be>
2024-07-03 15:45:32 +0200

    Improving charts view

--- src/main/kotlin/be/codewriter/melodymatrix/view/stage/chart/ChartsStage.kt
+++ src/main/kotlin/be/codewriter/melodymatrix/view/stage/chart/ChartsStage.kt
@@ -1,6 +1,5 @@
 package be.codewriter.melodymatrix.view.stage.chart
 
-import atlantafx.base.controls.ToggleSwitch
 import be.codewriter.melodymatrix.view.VisualizerStage
 import be.codewriter.melodymatrix.view.data.MidiData
 import be.codewriter.melodymatrix.view.data.PlayEvent
@@ -9,46 +8,44 @@ import javafx.scene.Node
 import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.layout.BorderPane
-import javafx.scene.layout.GridPane
+import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
 
 
 class ChartsStage : VisualizerStage() {
 
     var charts: MutableList<ChartVisualizer> = mutableListOf()
-    var grid: GridPane = GridPane()
+    var stackPane = StackPane()
 
     init {
-        grid.add(Button("Reset").apply {
-            setOnAction {
-                charts.forEach(ChartVisualizer::reset)
-            }
-        }, 0, 0)
-
-        charts.add(PieChartPlayedNotes())
-        charts.add(BarChartPlayedNotes())
-        charts.add(ConcentricPlayedNotes())
-        charts.add(CoxCombPlayedNotes())
-        charts.add(RidgeLineChartPlayedNotes())
-        charts.add(RadarChartPlayedNotes())
-
         val buttons = VBox().apply {
             spacing = 10.0
+            prefWidth = 200.0
+            minWidth = 200.0
+            maxWidth = 200.0
             children.addAll(
-                createButton("Pie chart", pie, false),
-                createButton("Bar chart", bar, false),
-                createButton("Concentric chart", concentric, false),
-                createButton("Cox Comb chart", coxcomb, false),
-                createButton("Ridge Line chart", ridgeline, true),
-                createButton("Radar Chart chart", radar, true)
+                Button("Reset").apply {
+                    prefWidth = 200.0
+                    minWidth = 200.0
+                    maxWidth = 200.0
+                    setOnAction {
+                        charts.forEach { c -> c.reset() }
+                    }
+                },
+                createButton("Pie chart", PieChartPlayedNotes()),
+                createButton("Bar chart", BarChartPlayedNotes()),
+                createButton("Concentric chart", ConcentricPlayedNotes()),
+                createButton("Cox Comb chart", CoxCombPlayedNotes()),
+                createButton("Ridge Line chart", RidgeLineChartPlayedNotes()),
+                createButton("Radar Chart chart", RadarChartPlayedNotes())
             )
         }
 
         title = "See your music in charts..."
         scene = Scene(BorderPane().apply {
             left = buttons
-            right = charts.get(0) as Node
-        }, 1400.0, 820.0)
+            right = stackPane
+        }, 800.0, 800.0)
 
         setOnCloseRequest {
             // Nothing needed here, but must be defined or will cause a problem when closing the window
@@ -56,18 +53,15 @@ class ChartsStage : VisualizerStage() {
     }
 
 
-    fun createButton(label: String, chart: Node, defaultAdded: Boolean): ToggleSwitch {
-        if (defaultAdded) {
-            charts.children.add(chart)
-        }
-        return ToggleSwitch(label).apply {
-            isSelected = defaultAdded
-            selectedProperty().addListener { _, _, newValue ->
-                if (newValue) {
-                    charts.children.add(chart)
-                } else {
-                    charts.children.remove(chart)
-                }
+    fun createButton(label: String, chart: ChartVisualizer): Button {
+        charts.add(chart)
+        stackPane.children.add(chart as Node)
+        return Button(label).apply {
+            prefWidth = 200.0
+            minWidth = 200.0
+            maxWidth = 200.0
+            setOnAction {
+                chart.toFront()
             }
         }
     }
