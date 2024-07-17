14a8b6c334fcebe1af12c709c6fc8768b5c598e5
Frank Delporte <frank@webtechie.be>
2024-07-17 11:38:58 +0200

    New piano backgrounds

--- src/main/kotlin/be/codewriter/melodymatrix/view/stage/piano/data/PianoBackgroundImage.kt
+++ src/main/kotlin/be/codewriter/melodymatrix/view/stage/piano/data/PianoBackgroundImage.kt
@@ -4,15 +4,15 @@ enum class PianoBackgroundImage(val label: String, val file: String) {
     NONE("None", "piano/backgrounds/none.png"),
     ABSTRACT_1("Abstract 1", "piano/backgrounds/abstract-1.png"),
     ABSTRACT_2("Abstract 2", "piano/backgrounds/abstract-2.png"),
-    CAR_1("Car 1", "piano/backgrounds/car-1.png"),
+    ABSTRACT_3("Abstract 3", "piano/backgrounds/abstract-3.png"),
     GRADIENT_1("Gradient 1", "piano/backgrounds/gradient-1.png"),
 
     //GRADIENT_2("Gradient 2", "piano/backgrounds/gradient-2.png"),
     GRADIENT_3("Gradient 3", "piano/backgrounds/gradient-3.png"),
     GRADIENT_4("Gradient 4", "piano/backgrounds/gradient-4.png"),
-    LINES_1("Lines 1", "piano/backgrounds/lines-1.png"),
     MOUNTAINS("Mountains", "piano/backgrounds/mountains-1.png"),
     NOTES_1("Notes 1", "piano/backgrounds/notes-1.png"),
-    SUN_1("Sund 1", "piano/backgrounds/sun-1.png"),
-    SUN_2("Sund 2", "piano/backgrounds/sun-2.png")
+    SUN_1("Sun 1", "piano/backgrounds/sun-1.png"),
+    SUN_2("Sun 2", "piano/backgrounds/sun-2.png"),
+    SUN_3("Sun 3", "piano/backgrounds/sun-3.png"),
 }
