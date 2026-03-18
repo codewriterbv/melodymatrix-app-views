2aa21e95cfc3dccef349658504c7fe03cd9fd358
Frank Delporte <frank@webtechie.be>
2026-03-18 09:52:02 +0100

    Code restructuring, better event dispatching, added chord events

---
+++ src/main/kotlin/be/codewriter/melodymatrix/view/definition/ChordAlteration.kt
@@ -0,0 +1,8 @@
+package be.codewriter.melodymatrix.view.definition
+
+enum class ChordAlteration(val label: String) {
+    NONE(""),
+    FLAT_FIFTH("b5"),
+    SHARP_FIFTH("#5")
+}
+
---
+++ src/main/kotlin/be/codewriter/melodymatrix/view/definition/ChordExtension.kt
@@ -0,0 +1,9 @@
+package be.codewriter.melodymatrix.view.definition
+
+enum class ChordExtension(val label: String) {
+    NONE(""),
+    MAJOR_SEVENTH("major 7th"),
+    MINOR_SEVENTH("minor 7th"),
+    DIMINISHED_SEVENTH("diminished 7th")
+}
+
